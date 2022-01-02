c8533ec50b16e76bd1b770567d1a38d363ea07e2
Enock Simiyu <60577078+enock295simiyu@users.noreply.github.com>
2022-01-02 12:14:30 +0300

    Home page is working

--- models.py
+++ models.py
@@ -1,4 +1,22 @@
-from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///premium_calculator_database.sqlite3'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, username, email, first_name, last_name, password):
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
