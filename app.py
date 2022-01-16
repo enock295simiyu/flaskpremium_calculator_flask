bd7104a86803daae5ecec72cc609541de7fca053
enock295simiyu <enocksimiyu8@gmail.com>
2022-01-16 16:16:47 +0300

    registration app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
--- models.py
+++ models.py
@@ -1,22 +1,41 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+from datetime import datetime
 
-from app import app
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import app, login_manager
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///premium_calculator_database.sqlite3'
 db = SQLAlchemy(app)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
-class User(db.Model):
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    password = db.Column(db.String(100))
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    email = db.Column(db.String(150), unique=True, index=True)
+    first_name = db.Column(db.String(50), index=True, unique=True)
+    last_name = db.Column(db.String(150), unique=True, index=True)
+    password_hash = db.Column(db.String(150))
+    joined_at = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
 
-    def __init__(self, username, email, first_name, last_name, password):
+    def __init__(self, username, email=None, first_name=None, last_name=None):
         self.username = username
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
-        self.password = password
+        self.email = email
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
