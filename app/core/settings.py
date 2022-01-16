4b7c2e7356c48e10adb4bd97d8f1f42749e39e58
enock295simiyu <enocksimiyu8@gmail.com>
2022-01-16 18:53:29 +0300

    Fixing placeholder and static in login and register

--- app/core/settings.py
+++ app/core/settings.py
@@ -2,16 +2,14 @@ import os
 
 from flask_sqlalchemy import SQLAlchemy
 
-from config import SQLALCHEMY_DATABASE_URI
 from app import app
+from config import SQLALCHEMY_DATABASE_URI
 
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+
 def return_db():
     return db
-
-
-
